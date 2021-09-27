3788b86e2db9ef8c26a9f08c233f09bc46bb7269
MD <alaminronybc@gmail.com>
2021-09-28 01:58:08 +0600

    up

---
+++ double-equal.js
@@ -0,0 +1,55 @@
+
+
+//OutPut: Condition is false
+
+// const first = 2;
+// const second = 3;
+// if(first == second){
+//     console.log("Condition is True");
+// }
+// else{
+//     console.log("Condition is false");
+// }
+
+
+
+
+//OutPut:Condition is True
+
+// const first = 2;
+// const second = 2;
+// if(first == second){
+//     console.log("Condition is True");
+// }
+// else{
+//     console.log("Condition is false");
+// }
+
+
+
+
+//OutPut:Condition is True
+
+// Double Equal == is check the value os its true
+// const first = 2;
+// const second = "2";
+// if(first == second){
+//     console.log("Condition is True");
+// }
+// else{
+//     console.log("Condition is false");
+// }
+
+
+
+
+//OutPut:Condition is false
+// Three Equal === is check the value and data type os its false bcz data type string and number
+const first = 2;
+const second = "2";
+if(first === second){
+    console.log("Condition is True");
+}
+else{
+    console.log("Condition is false");
+}
---
+++ null.js
@@ -0,0 +1,7 @@
+// function add(num1, num2){
+// console.log(num1 + num2);
+// return add;
+// }
+
+// const result = add(15, 15);
+// console.log(result);
