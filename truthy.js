636d0a6e6f99529718aec6f17a011e48008acb8a
MD <alaminronybc@gmail.com>
2021-09-28 01:19:54 +0600

    true-false

---
+++ truthy.js
@@ -0,0 +1,63 @@
+// const age = 4;
+// if(age > 0){
+//     console.log("Condition is true");
+// }
+// else{
+//     console.log("Condition is false");
+// }
+
+
+
+// const age = 4;               //OutPut: Condition is true
+// if(age){
+//     console.log("Condition is true");
+// }
+// else{
+//     console.log("Condition is false");
+// }
+
+
+
+// const age = 0;           //OutPut: Condition is false
+// if(age ){
+//     console.log("Condition is true");
+// }
+// else{
+//     console.log("Condition is false");
+// }
+
+
+// const name = "Rony";           //OutPut: Condition is true
+// if(name.length){
+//     console.log("Condition is true");
+// }
+// else{
+ //     console.log("Condition is false");
+// }
+
+
+// const name = "Rony";           //OutPut: Condition is true
+// if(name){
+//     console.log("Condition is true");
+// }
+// else{
+//     console.log("Condition is false");
+// }
+
+
+
+// const name = "";           //OutPut: Condition is false
+// if(name){
+//     console.log("Condition is true");
+// }
+// else{
+//     console.log("Condition is false");
+// }
+
+let name = null;         //OutPut: Condition is false
+if(name){
+    console.log("Condition is true");
+}
+else{
+    console.log("Condition is false");
+}
