58017b4989459ac1c90b61a7796359fb2fed45c3
MD <alaminronybc@gmail.com>
2021-09-28 18:21:34 +0600

    ok

---
+++ array-slice.js
@@ -0,0 +1,18 @@
+const num = [2, 8, 6, 4, 5, 9, 7, 1, 3];
+const part =num.slice(2, 6);
+// console.log(part);
+// console.log(num);
+
+
+
+const num2 = [2, 8, 6, 4, 5, 9, 7, 1, 3];
+const part2 =num2.splice(2, 5,70);
+// console.log(part2);
+// console.log(num2);
+
+
+const num3 = [2, 8, 6, 4, 5, 9, 7, 1, 3];
+const part3 =num3.join(",");
+//const part3 =num3.join(" ");
+//const part3 =num3.join("ami");
+console.log(part3);
